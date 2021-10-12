457d8cabdc7539ece3db28b3165e82b79b615f70
nabe-kurage <tabiflag@gmail.com>
2021-10-13 00:48:29 +0900

    set black first piece

--- src/App.js
+++ src/App.js
@@ -2,11 +2,21 @@ import React from "react";
 import "./App.css";
 
 const squareNum = { column: 8, row: 8 };
+const defaultPieceSet = {
+    white: [
+        [3, 3],
+        [4, 4],
+    ],
+    black: [
+        [3, 4],
+        [4, 3],
+    ],
+};
 
 function App() {
     const columns = [];
     for (let i = 0; i < squareNum.column; i++) {
-        columns.push(<Column rowNum={i} />);
+        columns.push(<Column columnNum={i} />);
     }
 
     return (
@@ -16,21 +26,43 @@ function App() {
     );
 }
 
+// 縦1ライン
 class Column extends React.Component {
     render() {
         const squares = [];
         for (let i = 0; i < squareNum.row; i++) {
-            squares.push(<div className="square" data-rowNum={i}></div>);
+            squares.push(
+                <Square columnNum={this.props.columnNum} rowNum={i} />
+            );
         }
 
+        return <div className="column">{squares}</div>;
+    }
+}
+
+// 横1マス
+class Square extends React.Component {
+    // TODO: 初期セットをおく
+    checkFirstSet(column, row) {
+        if ((column === 4 && row === 4) || (column === 3 && row === 3)) {
+            return <Piece />;
+        }
+    }
+
+    render() {
         return (
-            <div className="columns" data-rowNum={this.props.rowNum}>
-                {squares}
+            <div
+                className="square"
+                data-column={this.props.columnNum}
+                data-rowNum={this.props.rowNum}
+            >
+                {this.checkFirstSet(this.props.columnNum, this.props.rowNum)}
             </div>
         );
     }
 }
 
+// コマ
 class Piece extends React.Component {
     render() {
         return <div className="piece piece--black"></div>;
